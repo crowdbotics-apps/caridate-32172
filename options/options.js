c462c889880de3078e1bf59b19127efc27237e7f
Crowdbotics <team@crowdbotics.com>
2021-11-27 03:21:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "caridate_32172",
+  url: "https://caridate_32172.botics.co",
+  api: "https://caridate_32172.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
